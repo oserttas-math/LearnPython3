4150adae443a15ba198488c443a96ac501f4b56c
ozkan serttas <30179956+oserttas-math@users.noreply.github.com>
2019-12-06 21:47:50 -0600

    Update facet_plot.py

--- Snippets/facet_plot.py
+++ Snippets/facet_plot.py
@@ -20,3 +20,24 @@ g.set(xlabel = 'Month', ylabel = '')
 axes = plt.gca()
 _ = axes.set_ylim([0, d.year.max()])
 plt.tight_layout()
+
+
+# groupby and facet
+filtered  = data[data['earliest_cr_line_year'] > 2008]
+group = filtered.groupby(['grade', 'earliest_cr_line_year']).agg([np.mean])
+
+graph_df = group['int_rate'].reset_index()
+
+import seaborn as sns
+import matplotlib
+
+plt.style.use('fivethirtyeight')
+plt.suptitle('bold figure suptitle', fontsize=14, fontweight='bold')
+
+sns.set_style("whitegrid")
+g = sns.FacetGrid(graph_df, col="grade", col_wrap = 2)
+g = g.map(sns.pointplot, "earliest_cr_line_year", "mean")
+g.set(xlabel = 'Year', ylabel = '')
+axes = plt.gca()
+axes.set_ylim([0, graph_df['mean'].max()])
+_ = plt.tight_layout()
