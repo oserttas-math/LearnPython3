8b74cfb243fbb0ccd79a28b586b46f9d450d6b3e
ozkan serttas <30179956+oserttas-math@users.noreply.github.com>
2019-12-06 21:46:29 -0600

    Update barplot.py

--- Snippets/barplot.py
+++ Snippets/barplot.py
@@ -14,3 +14,21 @@ def emp_dur_graph(graph_title):
     _ = ax.set_xticklabels(ax.get_xticklabels(), rotation=0)
     
 emp_dur_graph('Distribution of employment length for issued loans')
+
+
+# second option with groupby
+
+group = data.groupby('grade').agg([np.mean])
+loan_amt_mean = group['loan_amnt'].reset_index()
+
+import seaborn as sns
+import matplotlib
+
+plt.style.use('fivethirtyeight')
+
+sns.set_style("whitegrid")
+ax = sns.barplot(y = "mean", x = 'grade', data=loan_amt_mean)
+ax.set(xlabel = '', ylabel = '', title = 'Average amount loaned, by loan grade')
+ax.get_yaxis().set_major_formatter(
+matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
+_ = ax.set_xticklabels(ax.get_xticklabels(), rotation=0)
