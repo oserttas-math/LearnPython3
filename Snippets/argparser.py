47b28cf0e2c439f641e99e1514412721bebcfe84
ozkan serttas <30179956+oserttas-math@users.noreply.github.com>
2020-08-17 23:20:44 -0500

    Create argparser.py

---
+++ Snippets/argparser.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+import argparse
+
+def run(args):
+	filename = args.input # these match the "dest": dest="input"
+	output_filename = args.output # from dest="output"
+	qual = args.quality_score # default is I
+
+	# Do stuff
+
+
+def main():
+	parser=argparse.ArgumentParser(description="Convert a fastA file to a FastQ file")
+	parser.add_argument("-in",help="fasta input file" ,dest="input", type=str, required=True)
+	parser.add_argument("-out",help="fastq output filename" ,dest="output", type=str, required=True)
+	parser.add_argument("-q",help="Quality score to fill in (since fasta doesn't have quality scores but fastq needs them. Default=I" ,dest="quality_score", type=str, default="I")
+	parser.set_defaults(func=run)
+	args=parser.parse_args()
+	args.func(args)
+
+if __name__=="__main__":
+	main()
